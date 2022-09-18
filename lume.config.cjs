1eaa943b94bd55444fda394bc41bcc96cb2199b9
Joe Pea <joe@trusktr.io>
2022-09-17 17:04:59 -0700

    WIP replace @lume/variable with classy-solid and update to stage 3 decorators

--- lume.config.cjs
+++ lume.config.cjs
@@ -1,3 +1,12 @@
 module.exports = {
 	globalName: 'LUME',
 }
+
+/*
+TODO:
+
+@lume/element:
+- replace __propsSetAtLeastOnce__ with updated API from classy-solid
+- replace __reactifiedProps__ with updated API from classy-solid
+
+*/
